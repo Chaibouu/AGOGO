4f8de2f3a60225f9d6e72a900ec6fc0acec64866
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-08-27 11:18:23 +0100

    agogo

---
+++ index.js
@@ -0,0 +1,6 @@
+let secs = document.querySelector('.secs');
+let cappucino = document.querySelector('.cappucino');
+let the = document.querySelector('.the');
+let petitdejeuner = document.querySelector('.petitdejeuner');
+let dejeuner = document.querySelector('.dejeuner');
+let entrer = document.querySelector('.entrer');
