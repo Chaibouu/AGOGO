11af079e4cbfcf82921f7c127a48d9192c21f6e8
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-08-30 00:16:11 +0100

    add blague

--- index.js
+++ index.js
@@ -3,90 +3,122 @@ let cappucino = document.querySelector('.cappucino');
 let the = document.querySelector('.the');
 let petitdejeuner = document.querySelector('.petitdejeuner');
 let dejeuner = document.querySelector('.dejeuner');
-let entrer = document.querySelector('.entrer').value;
+let entrer = document.querySelector('.entrer');
 let time = document.querySelector('.time');
+let containertempss = document.querySelector('.containertempss');
+let form =  document.querySelector('#form');
+let tempsrestent = document.querySelector('.tempsrestent');
+
 
   let heures = 0;
   let minutes = 0;
   let secondes = 0;
   let timeout;
-
+  let entrerer;
   
-
-
+ function BeBAckAt(minutes){
+  let min = minutes * 60 * 1000;
+  let back = new Date(new Date().getTime() + min);
+  let options = {hour: 'numeric', minute: 'numeric',second: 'numeric' };
+  let at = back.toLocaleTimeString('fr-FR',options);
+  return at;
+ }
+ 
  const chronos = ()=>{
-  // let x = setInterval(()=>{
-    heures = parseInt(heures);
-    minutes = parseInt(minutes);
-    secondes = parseInt(secondes);
-    secondes--;
-    // if (heures == 0 && minutes == 0 && secondes == 0) {
-    //   time.classList.add('hidden');
-    // }
-    // else{
-    //  time.classList.remove('hidden');
-    // }
-    if (minutes == 0 && heures > 0) {
-     heures--;
-     minutes = minutes + 59;
-   }
-    if (secondes == 0 && minutes > 0) {
-      minutes--;
-      secondes = 59;
-    }
-   if (secondes <= 0) {
-     secondes = 0;
-   }
-    if (secondes < 10) {
-     secondes = "0" + secondes;
-    }
-    if (minutes < 10) {
-     minutes = "0" + minutes;
-    }
-    if (heures < 10) {
-     heures = "0" + heures;
+
+    if (minutes > 60) {
+      heures = Math.floor(minutes/60);
+      minutes = Math.floor(minutes % 60);
     }
 
-    time.textContent = `${heures}:${minutes}:${secondes}`
-    timeout = setTimeout(chronos,1000);
-//  },1000)
+    if (heures == 0 && minutes == 0 && secondes == 0) {
+      clearTimeout(timeout);
+      containertempss.style.visibility = 'hidden';
+    }
+    else{
+      containertempss.style.visibility = 'visible';
+      heures = parseInt(heures);
+      minutes = parseInt(minutes);
+      secondes = parseInt(secondes);
+      secondes--;
+  
+      if (minutes == 0 && heures > 0) {
+        heures--;
+        minutes = minutes + 59;
+      }
+       if (secondes == 0 && minutes > 0) {
+         minutes--;
+         secondes = 59;
+       }
+      if (secondes <= 0) {
+        secondes = 0;
+      }
+       if (secondes < 10) {
+        secondes = "0" + secondes;
+       }
+       if (minutes < 10) {
+        minutes = "0" + minutes;
+       }
+       if (heures < 10) {
+        heures = "0" + heures;
+       }
+   
+       time.textContent = `${heures}:${minutes}:${secondes}`
+       
+       timeout = setTimeout(chronos,1000);
+    }
 };
 secs.addEventListener('click',()=>{
   clearTimeout(timeout);
   heures = 0;
   minutes = 0;
   secondes = 20;
+  tempsrestent.textContent = "Be Back At " + BeBAckAt(minutes);
   chronos();
 });
 cappucino.addEventListener('click',()=>{
   clearTimeout(timeout);
   heures = 0;
-  minutes = 4;
-  secondes = 60;
+  minutes = 5;
+  secondes = 1;
   chronos();
 });
 the.addEventListener('click',()=>{
   clearTimeout(timeout);
   heures = 0;
-  minutes = 14;
-  secondes = 60;
+  minutes = 15;
+  secondes = 1;
+  tempsrestent.textContent = "Be Back At " + BeBAckAt(minutes);
   chronos();
 });
 petitdejeuner.addEventListener('click',()=>{
   clearTimeout(timeout);
   heures = 0;
-  minutes = 19;
-  secondes = 60;
+  minutes = 20;
+  secondes = 1;
+  tempsrestent.textContent = "Be Back At " + BeBAckAt(minutes);
   chronos();
 });
 dejeuner.addEventListener('click',()=>{
   clearTimeout(timeout);
   heures = 0;
-  minutes = 29;
-  secondes = 60;
+  minutes = 30;
+  secondes = 1;
+  tempsrestent.textContent = "Be Back At " + BeBAckAt(minutes);
   chronos();
 });
 
-entrer.addEventListener("input",(e) =>{
-  valeur = e.target.value;
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  let valeur = parseFloat(entrer.value);
+  minutes = valeur;
+  secondes = 1;
+
+  clearTimeout(timeout);
+  tempsrestent.textContent = "Be Back At " + BeBAckAt(minutes);
+  chronos();
+  
 })
+
+
+
